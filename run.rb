5409b54a30bdec13dec6457c4a2f7bcb30de6743
Marco Damaceno <maadamaceno@gmail.com>
2022-12-29 09:31:20 -0300

    feat: first commit

---
+++ run.rb
@@ -0,0 +1,17 @@
+RAILS_KICKSTART_DIR = __dir__
+APP_PATH = ARGV.first
+
+puts "RUBY VERSION: #{`ruby -v`}"
+
+system("rails new #{APP_PATH} -d postgresql --css bootstrap")
+
+Dir.chdir(APP_PATH)
+
+system("bundle add devise bundler-audit brakeman view_component sassc-rails")
+system('bundle add rails_best_practices rubocop-rails license_finder --group "development"')
+system('bundle add rspec-rails --group "test"')
+
+system('bin/rails generate rspec:install')
+system('rm -rf test')
+
+puts "All done!"
