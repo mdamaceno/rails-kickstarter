fd969b675652c0ef18ca80e2aaf306b68fd66d25
Marco Damaceno <maadamaceno@gmail.com>
2022-12-29 09:42:35 -0300

    remove jbuilder and add pry

--- run.rb
+++ run.rb
@@ -10,6 +10,8 @@ Dir.chdir(APP_PATH)
 system("bundle add devise bundler-audit brakeman view_component sassc-rails")
 system('bundle add rails_best_practices rubocop-rails license_finder --group "development"')
 system('bundle add rspec-rails --group "test"')
+system('bundle add pry-rails --group "development test"')
+system('bundle remove jbuilder')
 
 system('bin/rails generate rspec:install')
 system('rm -rf test')
